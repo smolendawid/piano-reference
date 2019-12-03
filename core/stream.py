40b14fee3bb7cf393f990039d2803c2b7c3fc4be
Dawid Smoleń <smolendawid@gmail.com>
2019-12-03 14:06:36 +0100

    working reference

--- core/stream.py
+++ core/stream.py
@@ -8,37 +8,17 @@ class Stream:
         self.p = pyaudio.PyAudio()
         self.sample_rate = sample_rate
 
-        # for paFloat32 sample values must be in range [-1.0, 1.0]
-        self.stream = self.p.open(format=pyaudio.paFloat32,
-                                  channels=1,
-                                  rate=sample_rate,
-                                  output=True)
+        self.stream = self.p.open(format=pyaudio.paFloat32, channels=1, rate=sample_rate, output=True)
         self.samples = 0.
 
     def create_sine_tone(self, frequency, duration):
-        # generate samples, note conversion to float32 array
         self.samples = (np.sin(2 * np.pi * np.arange(self.sample_rate * duration) * frequency
                                / self.sample_rate)).astype(np.float32)
 
-    def play_sine_tone(self, stop, volume=1.):
-        """
-        :param frequency:
-        :param duration:
-        :param volume:
-        :param sample_rate:
-        :return:
-        """
-
-        # play. May repeat with different volume values (if done interactively)
-        while not stop:
-            self.stream.write(volume * self.samples)
-
-        self.p.terminate()
-        self.finish()
+    def play_sine_tone(self, volume=1.):
+        self.stream.write(volume * self.samples)
 
     def finish(self):
-        self.p.terminate()
         self.stream.stop_stream()
         self.stream.close()
-
-
+        self.p.terminate()
--- main.py
+++ main.py
@@ -1,42 +1,110 @@
 from __future__ import division
-import math
-import threading
+
 from tkinter import Button, Label, Tk
+import threading
+
+import pyaudio
+import numpy as np
+
 from core.stream import Stream
-import time
+from core.tone2frequency import tone2frequency
+from data.key_midi_mapping import midi_key_mapping
+
+
+class ThreadPlayer:
+    def __init__(self, **kwargs):
+        self.play = True
+        self.thread_kill = False
+        self.sample_rate = 44100
+        self.p = pyaudio.PyAudio()
+        self.stream = self.p.open(format=pyaudio.paFloat32, channels=1, rate=self.sample_rate, output=True)
+
+        self.samples = None
+
+    def create_sine_tone(self, frequency, duration):
+        self.samples = (np.sin(2 * np.pi * np.arange(self.sample_rate * duration) * frequency
+                               / self.sample_rate)).astype(np.float32)
+
+    def callback(self, in_data, frame_count, time_info, status):
+        return self.samples, pyaudio.paContinue
+
+    def start_sound(self, f):
+        self.p = pyaudio.PyAudio()
+        self.create_sine_tone(f, 1/f*10)
+        self.stream = self.p.open(format=pyaudio.paFloat32, channels=1, rate=self.sample_rate, output=True,
+                                  stream_callback=self.callback, frames_per_buffer=len(self.samples))
+        self.stream.start_stream()
+        # while True and not self.thread_kill:
+        #     if self.play:
+                # try:
+                # self.stream.write(1. * self.samples, exception_on_underflow=False)
+                # except:
+                #     print("thrown")
+                #     continue
+        # self.stream.finish()
+
+    def stop_sound(self):
+        self.play = False
+        self.stream.stop_stream()
+        self.stream.close()
+        self.p.terminate()
+
+    def start_thread(self, frequency):
+        self.play = True
+        self.start_sound(frequency)
+        # t1 = threading.Thread(target=self.start_sound, args=(frequency,))
+        # t1.start()
 
 
 class MainWindow:
     def __init__(self):
-        self.frequency = 100
+        self.tone = 49  # A4
+        self.frequency = 440
+        self.p1 = ThreadPlayer()
+        self.p2 = ThreadPlayer()
+
+    def _frequency_increment(self):
+        self.tone += 1
+        self.frequency = tone2frequency(self.tone)
+
+    def _frequency_decrement(self):
+        self.tone -= 1
+        self.frequency = tone2frequency(self.tone)
 
     def show(self):
         window = Tk()
         window.title("Piano reference")
         window.geometry('350x200')
 
-        lbl = Label(window, text="A4")
-        lbl.grid(column=2, row=1)
+        lbl_midi = Label(window, text="49")
+        lbl_key = Label(window, text="A4")
+        lbl_freq = Label(window, text="440.0")
 
-        running = threading.Event()
+        lbl_midi.grid(column=2, row=1)
+        lbl_key.grid(column=2, row=2)
+        lbl_freq.grid(column=2, row=3)
 
         def left_click(event):
-            running.clear()
-            running.set()
-            s = Stream(44100)
-            s.create_sine_tone(frequency, 1.)
-            thread = threading.Thread(target=s.play_sine_tone, args=(running, 1,))
-            thread.start()
-            lbl.configure(text=frequency)
+            self._frequency_decrement()
+            self.p1.stop_sound()
+            self.p2.stop_sound()
+            self.p1 = ThreadPlayer()
+            self.p2 = ThreadPlayer()
+            self.p1.start_thread(self.frequency)
+            lbl_midi.configure(text=self.tone)
+            lbl_key.configure(text=midi_key_mapping[self.tone])
+            lbl_freq.configure(text=self.frequency)
 
         def right_click(event):
-            running.clear()
-            running.set()
-            s = Stream(44100)
-            s.create_sine_tone(frequency, 1.)
-            thread = threading.Thread(target=s.play_sine_tone, args=(running, 1,))
-            thread.start()
-            lbl.configure(text=frequency)
+            self._frequency_increment()
+            self.p1.stop_sound()
+            self.p2.stop_sound()
+            self.p1 = ThreadPlayer()
+            self.p2 = ThreadPlayer()
+            self.p2.start_thread(self.frequency)
+            lbl_midi.configure(text=self.tone)
+            lbl_key.configure(text=midi_key_mapping[self.tone])
+            lbl_freq.configure(text=self.frequency)
 
         btn1 = Button(window, text="<<", command=lambda: left_click(None))
         btn2 = Button(window, text=">>", command=lambda: right_click(None))
@@ -48,25 +116,6 @@ class MainWindow:
         window.bind('<Right>', right_click)
 
         window.mainloop()
-
-
-class TestThread(threading.Thread):
-    def __init__(self, frequency):
-        """ constructor, setting initial variables """
-        self._stopevent = threading.Event()
-        self.frequency = frequency
-
-        threading.Thread.__init__(self)
-
-    def run(self):
-        s = Stream(44100)
-        s.create_sine_tone(self.frequency, 1.)
-        s.play_sine_tone(self._stopevent.isSet(), 1.)
-
-    def join(self, timeout=None):
-        """ Stop the thread. """
-        self._stopevent.set()
-        threading.Thread.join(self, timeout)
 
 
 if __name__ == '__main__':
